33e150dc53803520fc9776087cb60a112c5d0d91
Operations Research Engineering Software+ <alex@oresoftware.com>
2020-03-19 13:47:46 -0700

    squash-me

--- out/extension.js
+++ out/extension.js
@@ -1,9 +1,22 @@
-"use strict";
+'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
+const net = require("net");
+const json_stream_parser_1 = require("@oresoftware/json-stream-parser");
+const connections = new Set();
+const server = net.createServer(s => {
+    connections.add(s);
+    s.pipe(new json_stream_parser_1.JSONParser()).on('data', d => {
+        console.log('server received data:', d);
+        vscode.window.showInformationMessage('cprev server received data: ' + JSON.stringify(d));
+    });
+});
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+    server.listen(3119, () => {
+        vscode.window.showInformationMessage('cprev server is listening on port: ' + 3119);
+    });
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "cprev-vscode-extension" is now active!');
@@ -19,6 +32,20 @@ function activate(context) {
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {
+    if (!server.listening) {
+        vscode.window.showInformationMessage('cprev extension deactivated, server was not listening.');
+        return;
+    }
+    server.close(err => {
+        if (err) {
+            console.error(err);
+            vscode.window.showErrorMessage('Upon cprev extension deactivation, ' +
+                'we have an error closing cprev server: ' + err.message);
+            return;
+        }
+        vscode.window.showInformationMessage('cprev extension deactivated.');
+    });
+}
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,38 @@
+'use strict';
 
 import * as vscode from 'vscode';
+import * as net from 'net';
+import {JSONParser} from '@oresoftware/json-stream-parser';
+
+const connections = new Set<net.Socket>()
+
+
+const server  = net.createServer(s => {
+   connections.add(s);
+
+   s.pipe(new JSONParser()).on('data', d  => {
+		console.log('server received data:', d);
+		vscode.window.showInformationMessage('cprev server received data: ' + JSON.stringify(d));
+   });
+
+});
+
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+
+	server.listen(3119, () => {
+		vscode.window.showInformationMessage('cprev server is listening on port: ' + 3119);
+	});
+
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "cprev-vscode-extension" is now active!');
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -23,4 +48,24 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+
+	if(!server.listening){
+		vscode.window.showInformationMessage('cprev extension deactivated, server was not listening.');
+		return;
+	}
+
+
+  server.close(err => {
+
+	if(err){
+		console.error(err);
+		vscode.window.showErrorMessage('Upon cprev extension deactivation, ' + 
+		'we have an error closing cprev server: ' + err.message);
+		return;
+	}
+	
+	vscode.window.showInformationMessage('cprev extension deactivated.');
+
+  });
+}
